4199d93a63761a44efddad2e192045b2c4aaab71
austingw <senortinman@gmail.com>
2024-03-18 21:10:02 -0400

    Add new color theme to match wezterm

--- init.lua
+++ init.lua
@@ -92,7 +92,6 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
-vim.g.moonflyTransparent = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
--- lua/swag/plugins/colorthemes.lua
+++ lua/swag/plugins/colorthemes.lua
@@ -22,7 +22,21 @@ return {
     lazy = false,
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'moonfly'
+      --vim.cmd.colorscheme 'moonfly'
+    end,
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    config = function()
+      -- Default options
+      require('nightfox').setup {
+        options = {
+          transparent = true, -- Disable setting background
+        },
+      }
+    end,
+    init = function()
+      vim.cmd.colorscheme 'nightfox'
     end,
   },
 }
