80bea1c664b4ced2c867073bbe5874679e159aef
austingw <senortinman@gmail.com>
2024-03-22 23:22:48 -0400

    Add additional autocommands

--- lua/swag/config/autocmds.lua
+++ lua/swag/config/autocmds.lua
@@ -1,13 +1,51 @@
 --Epic autocommands go here
---  See `:help lua-guide-autocommands`
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = augroup 'kickstart-highlight-yank',
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'VimResized' }, {
+  group = augroup 'resize_splits',
+  desc = 'Resize splits when resizing the window',
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd 'tabdo wincmd ='
+    vim.cmd('tabnext ' .. current_tab)
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = augroup 'last_loc',
+  desc = 'Jump to the last location when opening a file',
+  callback = function(event)
+    local exclude = { 'gitcommit' }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
+  group = augroup 'checktime',
+  desc = 'Check if the file changed on focus',
+  callback = function()
+    if vim.o.buftype ~= 'nofile' then
+      vim.cmd 'checktime'
+    end
   end,
 })
